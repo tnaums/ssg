15309f10274ab50339c94142da2c5f1cc6dab1e5
tnaums <tanaumann@outlook.com>
2025-12-08 23:12:17 -0600

    chapter 2, lesson 1

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+from textnode import TextType
+
+def main():
+    tnode1 = TextNode("Some illin text", TextType.BOLD, "https://www.boot.dev")
+    print(tnode1)
+
+
+main()
